1c4cdaca63e46809227e34165b33332929cd2445
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-30 19:45:38 -0400

    Update node, adds add_value method

--- lib/node.rb
+++ lib/node.rb
@@ -1,12 +1,16 @@
 class Node
-  attr_reader :next_node
+  attr_reader :next_node, :value
 
-  def initialize(token)
-    @token = token
+  def initialize
+    @value = nil
     @next_node = nil
   end
 
   def add_next(next_item)
     @next_node = next_item
+  end
+
+  def add_value(value)
+    @value = value
   end
 end
