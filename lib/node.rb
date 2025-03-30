26ea783d46209d715fa2dff410e5f60618254b61
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-30 19:57:52 -0400

    Update node

--- lib/node.rb
+++ lib/node.rb
@@ -1,16 +1,12 @@
 class Node
   attr_reader :next_node, :value
 
-  def initialize
-    @value = nil
+  def initialize(value)
+    @value = value
     @next_node = nil
   end
 
   def add_next(next_item)
     @next_node = next_item
-  end
-
-  def add_value(value)
-    @value = value
   end
 end
