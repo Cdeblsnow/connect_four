e34a811b483e1ea3ef1dbcfa287b3aa2aa7f34d2
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-01 19:42:49 -0400

    Update

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,17 @@
 class Player
-  attr_reader :name, :points
+  attr_reader :name, :points, :token
 
   def initialize(name)
-    @token = ""
+    @token = nil
     @points = 0
     @name = name
   end
 
   def add_point
     @points += 1
+  end
+
+  def define_token(token)
+    @token = token
   end
 end
