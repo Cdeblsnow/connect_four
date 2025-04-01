b885b062c3cec4491a2dbb29fe39be32623c2b0d
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-01 19:34:12 -0400

    Update

--- spec/board_game_spec.rb
+++ spec/board_game_spec.rb
@@ -28,16 +28,4 @@ describe BoardGame do
       expect(players.length).to eq(4)
     end
   end
-
-  describe "#define_player_token" do
-    subject(:board_player_token) { described_class.new }
-    it "permits two players to pick their token" do
-      players = board_player_token.player_init
-      board_player_token.define_player_token(players)
-      allow($stdin).to receive(:gets).and_return("1\n")
-
-      tokens = ["\u{2648}", "\u{2649}", "\u{264A}", "\u{264B}"]
-      expect(players[1].token).to eq(tokens[1])
-    end
-  end
 end
