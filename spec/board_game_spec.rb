39d6b63f0ac2bff0e979bb4f8d08cca511ac99bb
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-02 18:05:02 -0400

    Add select_column test

--- spec/board_game_spec.rb
+++ spec/board_game_spec.rb
@@ -1,4 +1,4 @@
-require_relative "../lib/board_display"
+require_relative "../lib/board_game"
 
 describe BoardGame do
   describe "#player_init" do
@@ -26,6 +26,24 @@ describe BoardGame do
     it "create three players when input is 4" do
       players = board_player.player_init
       expect(players.length).to eq(4)
+    end
+  end
+
+  describe "#select_column" do
+    subject(:board_column) { described_class.new }
+    it "receive column number and add player token to the empty position" do
+      board_column.select_column(1, "p")
+      list = board_column.instance_variable_get(:@node_lists)
+      expect(list[1].head.value).to eq("p")
+    end
+
+    it "receive column number and add two different player tokens to the empty positions" do
+      board_column.select_column(1, "p")
+      board_column.select_column(1, "f")
+      list = board_column.instance_variable_get(:@node_lists)
+      node = list[1].head
+      expect(node.value).to eq("f") # head last element added
+      expect(node.next_node.value).to eq("p") # tail first element  added
     end
   end
 end
