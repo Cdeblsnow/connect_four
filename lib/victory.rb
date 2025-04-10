9853240537a7284df1c15afece9b54abcea2f508
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-09 20:52:19 -0400

    Feat:it is possible to win with l/r  diagonal

--- lib/victory.rb
+++ lib/victory.rb
@@ -1,6 +1,6 @@
 require "pry-byebug"
 module Victory
-  def vertical_win?(player_token)
+  def vertical_win?(player_token) # re check, I was not using node.value
     i = 0
     each_vertical_node do |node|
       if node == player_token
@@ -14,12 +14,26 @@ module Victory
     false
   end
 
-  def horizontal_win?(player_token)
+  def horizontal_win?(player_token) # re check, I was not using node.value
     i = 0
     each_horizontal_node do |node|
       if node == player_token
         i += 1
 
+      else
+        i = 0
+      end
+      return true if i == 4
+    end
+    false
+  end
+
+  def left_diagonal_win?(player_token)
+    i = 0
+    each_left_diagonal_node do |node|
+      if node.value == player_token
+        i += 1
+        puts "#{node.value} : #{i}" # to be removed
       else
         i = 0
       end
