ad472f3885524a2cd14bd5979dde55750d8a4385
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-09 20:51:46 -0400

    Add left diagonal win test

--- spec/board_game_spec.rb
+++ spec/board_game_spec.rb
@@ -57,7 +57,7 @@ describe BoardGame do
         token = "p"
         columns.each do |key, content|
           content.add_node("c") if key == 1
-          4.times { |i| content.add_node(token) } if key == 1
+          4.times { |i| content.add_node(token) } if key == 1 # rubocop:disable Lint/UnusedBlockArgument
         end
         expect(vertical_win.vertical_win?(token)).to eq(true)
       end
@@ -74,6 +74,23 @@ describe BoardGame do
         end
         expect(horizontal_win.horizontal_win?(token)).to eq(true)
       end
+    end
+  end
+
+  describe "#left_diagonal_win?" do
+    subject(:diagonal) { described_class.new }
+    let(:columns) { diagonal.instance_variable_get(:@node_lists) }
+    it "returns true when there are four of the same token in a diagonal" do
+      token = "t"
+      index = 0
+      columns.each_value do |content|
+        4.times do |i|
+          content.add_node(token) if i == (4 - index)
+          content.add_node("c") if i < 4
+        end
+        index += 1 if index < 5
+      end
+      expect(diagonal.left_diagonal_win?(token)).to eq(true)
     end
   end
 end
