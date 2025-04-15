f3df715ec51f21a9a89dd4f26ed4f9676278064a
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-15 18:01:32 -0400

    Remove test for deleted method

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,18 +2,10 @@ require_relative "../lib/node"
 
 describe Node do
   describe "#add_next" do
-    subject(:next_n) { described_class.new }
+    subject(:next_n) { described_class.new("test") }
     it "adds next node" do
       next_n.add_next(6)
       expect(next_n.next_node).to eq(6)
-    end
-  end
-
-  describe "#add_next" do
-    subject(:node_value) { described_class.new }
-    it "adds value to the current node" do
-      node_value.add_value(6)
-      expect(node_value.value).to eq(6)
     end
   end
 end
