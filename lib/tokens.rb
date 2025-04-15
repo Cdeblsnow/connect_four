4c40e726740d183f956f7c517c93f74db02243f0
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-04-15 18:14:11 -0400

    Delete unused method

--- lib/tokens.rb
+++ lib/tokens.rb
@@ -1,17 +1,10 @@
-class Tokens
-  def initialize
-    @tokens = ["\u{2648}", "\u{2649}", "\u{264A}", "\u{264B}"]
-  end
-
+module Tokens
   def define_player_token(player)
     puts "#{player.name} select your token"
+    p @tokens
     selection = gets.chomp.to_i - 1
     player.define_token(@tokens[selection])
     @tokens.delete_at(selection)
     player.token
-  end
-
-  def reset
-    @tokens = ["\u{2648}", "\u{2649}", "\u{264A}", "\u{264B}"]
   end
 end
